50d575e60e204bf52378fb0ca3084e7498674b49
mahmudunnaby <mahmudunnabyemon0@gmail.com>
2022-05-30 13:27:21 +0600

    adding heroku

--- index.js
+++ index.js
@@ -173,7 +173,11 @@ async function run() {
 
 run().catch(console.dir);
 
+app.get('/', (req, res) => {
 
+    res.send('runing manufacture server')
+
+})
 
 app.listen(port, () => {
     console.log('listning to port', port);
